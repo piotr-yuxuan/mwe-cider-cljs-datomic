53a50813b09421c5a9582c3f9a1558b6e8a4763d
piotr-yuxuan <pierre2b@adikteev.com>
2018-04-09 00:47:19 +0200

    Add datomic-free as a dependency
    Now when I type `lein repl`, I get that stacktrace:
    
    https://gist.github.com/piotr-yuxuan/981e13ba052ee44a085a0e9420e3ac26

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [org.clojure/core.async  "0.4.474"]]
+                 [org.clojure/core.async  "0.4.474"]
+                 [com.datomic/datomic-free "0.9.5697"]]
 
   :plugins [[lein-figwheel "0.5.15"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
